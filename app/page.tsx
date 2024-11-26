8522e5a949635c1ee2a227907281206d16f2b8e7
Shanmukh Anaparthi <110324769+Shanmukh025@users.noreply.github.com>
2024-11-27 00:56:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             <Link
               key={item.href}
               href={item.href}
-              className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
+              className="duration-200 text-zinc-400 hover:text-zinc-100"
             >
               {item.name}
             </Link>
